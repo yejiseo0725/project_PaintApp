dc30e14004a345a38d948e4d93c3be201239ec12
YEJI SEO <yejiseo0725@gmail.com>
2024-09-06 14:23:36 +0900

    how to make person

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(210, 200, 15, 100);
+ctx.fillRect(350, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
+
+ctx.arc(290, 130, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "turquoise";
+ctx.arc(260 + 10, 120, 8, Math.PI, 2 * Math.PI);
+ctx.arc(300 + 10, 120, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
