0645e4954db6aa1552aa8d9870058974f729b853
YEJI SEO <yejiseo0725@gmail.com>
2024-09-06 14:40:30 +0900

    canvas mouseover

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+ctx.lineWidth = 2;
 
-ctx.fillRect(210, 200, 15, 100);
-ctx.fillRect(350, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
+const colors = [
+  "#d63031",
+  "#e17055",
+  "#fdcb6e",
+  "#00b894",
+  "#00cec9",
+  "#0984e3",
+  "#6c5ce7",
+  "#fd79a8",
+];
 
-ctx.arc(290, 130, 50, 0, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.fillStyle = "turquoise";
-ctx.arc(260 + 10, 120, 8, Math.PI, 2 * Math.PI);
-ctx.arc(300 + 10, 120, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+canvas.addEventListener("mousemove", onClick);
