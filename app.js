8ef2e4fa0e5bb850d9cf229d3570539c8494b91d
YEJI SEO <yejiseo0725@gmail.com>
2024-09-09 13:43:58 +0900

    change line width

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
-const colors = [
-  "#d63031",
-  "#e17055",
-  "#fdcb6e",
-  "#00b894",
-  "#00cec9",
-  "#0984e3",
-  "#6c5ce7",
-  "#fd79a8",
-];
+let isPainting = false;
 
-function onClick(event) {
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
   ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function startPainting(event) {
+  isPainting = true;
+}
+
+function cancelPainting(event) {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+
+// const colors = [
+//   "#d63031",
+//   "#e17055",
+//   "#fdcb6e",
+//   "#00b894",
+//   "#00cec9",
+//   "#0984e3",
+//   "#6c5ce7",
+//   "#fd79a8",
+// ];
+
+// function onClick(event) {
+//   ctx.beginPath();
+//   ctx.moveTo(0, 0);
+//   const color = colors[Math.floor(Math.random() * colors.length)];
+//   ctx.strokeStyle = color;
+//   ctx.lineTo(event.offsetX, event.offsetY);
+//   ctx.stroke();
+// }
+
+// canvas.addEventListener("mousemove", onClick);
